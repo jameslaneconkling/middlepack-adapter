192ebd8c6676ed22b0ac31380d0f2f525b8e35f1
James Conkling <james.lane.conkling@gmail.com>
2018-02-10 17:38:27 -0500

    simplify IRange protocol

--- src/middlepack_adapter/range.clj
+++ src/middlepack_adapter/range.clj
@@ -15,16 +15,7 @@
     [{:keys [from to] :or {from 0}}]
     (hash-map :offset from :limit (- (inc to) from)))
 
-  java.lang.Integer
-  (range->list
-    [element]
-    (vector element))
-  (range->offset-limit
-    [element]
-    (hash-map :offset element :limit 1))
-
-  ;; TODO - is there a more concise pattern?
-  java.lang.Long
+  java.lang.Number
   (range->list
     [element]
     (vector element))
